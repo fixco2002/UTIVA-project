b53d6187704d144fa3c24f254f09f3768a22c75d
fixco2002 <128331814+fixco2002@users.noreply.github.com>
2023-03-20 02:16:29 +0000

    Add files via upload

--- --Joke Obasanmi data Science Postgres Assignment2.sql
+++ --Joke Obasanmi data Science Postgres Assignment2.sql
@@ -111,5 +111,489 @@ FROM
 	all_world_cup
 ;
 
-/*Q3 Country with the highest and lowest games played*/
 
+/*Q3 Country with the highest and lowest appearance*/
+
+
+/*Q4 Country with the highest and lowest games played*/
+
+        WITH
+            all_world_cup AS (
+            SELECT 
+            'wc 2002' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2002
+        UNION ALL
+        SELECT 
+            'wc 2006' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2006
+        UNION ALL
+        SELECT 
+            'wc 2010' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2010
+        UNION ALL
+        SELECT 
+            'wc 2014' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2014
+        UNION ALL
+        SELECT 
+            'wc 2018' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2018
+            )
+            
+SELECT  
+    team, 
+	games_played
+FROM
+	all_world_cup
+ORDER BY
+	games_played DESC;
+
+
+/*Q5 Country with the highest and lowest games won*/
+        WITH
+            all_world_cup AS (
+            SELECT 
+            'wc 2002' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2002
+        UNION ALL
+        SELECT 
+            'wc 2006' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2006
+        UNION ALL
+        SELECT 
+            'wc 2010' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2010
+        UNION ALL
+        SELECT 
+            'wc 2014' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2014
+        UNION ALL
+        SELECT 
+            'wc 2018' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2018
+            )
+	
+SELECT  
+    team, 
+	win
+FROM
+	all_world_cup
+ORDER BY
+	win DESC;
+
+
+/*Q6 Country with the hihest games lost*/
+        WITH
+            all_world_cup AS (
+            SELECT 
+            'wc 2002' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2002
+        UNION ALL
+        SELECT 
+            'wc 2006' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2006
+        UNION ALL
+        SELECT 
+            'wc 2010' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2010
+        UNION ALL
+        SELECT 
+            'wc 2014' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2014
+        UNION ALL
+        SELECT 
+            'wc 2018' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2018
+            )
+            
+SELECT  
+    team, 
+	loss
+FROM
+	all_world_cup
+ORDER BY
+	loss DESC;
+
+
+    /*Q7 Country with the highest and lowest goals for*/
+        WITH
+            all_world_cup AS (
+            SELECT 
+            'wc 2002' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2002
+        UNION ALL
+        SELECT 
+            'wc 2006' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2006
+        UNION ALL
+        SELECT 
+            'wc 2010' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2010
+        UNION ALL
+        SELECT 
+            'wc 2014' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2014
+        UNION ALL
+        SELECT 
+            'wc 2018' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2018
+            )
+	
+SELECT  
+    team, 
+	goals_for
+FROM
+	all_world_cup
+ORDER BY
+	goals_for DESC;
+
+
+/*Q8 countrty with the highest and lowest goals against*/
+        WITH
+            all_world_cup AS (
+            SELECT 
+            'wc 2002' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2002
+        UNION ALL
+        SELECT 
+            'wc 2006' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2006
+        UNION ALL
+        SELECT 
+            'wc 2010' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2010
+        UNION ALL
+        SELECT 
+            'wc 2014' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2014
+        UNION ALL
+        SELECT 
+            'wc 2018' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2018
+            )
+	
+SELECT  
+    team, 
+	goals_against
+FROM
+	all_world_cup
+ORDER BY
+	goals_against DESC;
+
+
+    /*Q9 Country with tghe highest and lowest goals difference*/
+WITH
+            all_world_cup AS (
+            SELECT 
+            'wc 2002' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against,
+			goals_difference
+        FROM fifa_2002
+        UNION ALL
+        SELECT 
+            'wc 2006' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against,
+			goals_difference
+        FROM fifa_2006
+        UNION ALL
+        SELECT 
+            'wc 2010' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against,
+			goals_difference
+        FROM fifa_2010
+        UNION ALL
+        SELECT 
+            'wc 2014' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against,
+			goals_difference
+        FROM fifa_2014
+        UNION ALL
+        SELECT 
+            'wc 2018' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against,
+			goals_difference
+        FROM fifa_2018
+            )
+	
+SELECT  
+    team, 
+	goals_difference
+FROM
+	all_world_cup
+ORDER BY
+	goals_difference DESC;
+
+
+    /*Q10 Country with the highest and lowest points*/
+WITH
+            all_world_cup AS (
+            SELECT 
+            'wc 2002' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2002
+        UNION ALL
+        SELECT 
+            'wc 2006' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2006
+        UNION ALL
+        SELECT 
+            'wc 2010' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2010
+        UNION ALL
+        SELECT 
+            'wc 2014' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2014
+        UNION ALL
+        SELECT 
+            'wc 2018' AS worldcupyear,
+            team,
+            points,
+            games_played,
+            win,
+            loss,
+            goals_for,
+            goals_against
+        FROM fifa_2018
+            )
+	
+SELECT  
+    team, 
+	SUM(points)
+FROM
+	all_world_cup
+GROUP BY
+	team
+ORDER BY
+	SUM(points) DESC ;
